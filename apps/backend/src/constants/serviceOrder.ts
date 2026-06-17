89bc83142d1569160efaae263bce22f95d352236
matheusqueiroz92 <matheus_giga@hotmail.com>
2026-06-17 12:10:24 -0300

    fix: edit service order start

--- apps/backend/src/constants/serviceOrder.ts
+++ apps/backend/src/constants/serviceOrder.ts
@@ -2,7 +2,7 @@
 export const SERVICE_ORDER_COUNTER_ID = "serviceOrder";
 
 /** Primeiro número de ordem de serviço gerado automaticamente */
-export const SERVICE_ORDER_START = 106250;
+export const SERVICE_ORDER_START = 106251;
 
 /** Valor inicial do contador para que o próximo número seja SERVICE_ORDER_START */
 export const SERVICE_ORDER_COUNTER_INITIAL = SERVICE_ORDER_START - 1;
